04fa3c7d70453f703cdb7725d0b19455b5b3c7a6
Peter Goulding <peterngoulding@gmail.com>
2019-06-11 21:19:19 -0400

    "main rb file originally committed"

---
+++ who_watir.rb
@@ -0,0 +1,282 @@
+require 'watir'
+
+$browser = Watir::Browser.new
+
+$browser.goto('https://www.whoscored.com/Regions/252/Tournaments/2/England-Premier-League')
+
+def file_out(string)
+open('myfile.json', 'a') { |f|
+    f.puts string
+  }
+end
+
+def wait_click_e(xpath)
+    $browser.element(:xpath => xpath).wait_until(&:present?).click
+end
+
+def wait_for_e(xpath)
+    $browser.element(:xpath => xpath).wait_until(&:present?)
+end
+
+def select_stats_list(list, value)
+    case list
+        when "category" then $browser.select_list(:xpath => '//*[@id="category"]').select(value)
+            file_out('{ "' + value + '": ')
+        else $browser.select_list(:xpath => '//*[@id="statsAccumulationType"]').select(value)
+    end
+    
+
+end
+
+def get_full_table()
+    stats_table = '//*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]'
+    #puts stats_table
+    wait_click_e('//*[@id="player-tournament-stats-options"]/li[5]/a')
+
+    row_count = $browser.element(:xpath => '//*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]').wait_until.trs.count
+    #puts row_count
+
+    row = $browser.element(:xpath => '//*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]').wait_until
+    sleep(0.5)
+    row.trs.each do |tr|
+        sleep(0.5)
+        tr.each do |cell|
+            cell_key = cell.class_name.gsub("sorted", "")
+            cell_key = cell_key.gsub(" ", "")
+            cell_value = cell.inner_html.gsub("-", "0")
+            cell_text = cell.inner_text
+            
+            if cell.inner_html.length > 25
+                unless cell_text.include? "Total"
+                    file_out('{ "' + cell_text + '": ')
+                end
+            elsif cell.class_name.length < 1
+                next
+            elsif cell_value.include?("strong")
+                next
+            elsif cell.inner_html != nil
+                file_out('{ "' + cell_key + '": "' + cell_value + '" }')
+                if cell_key.include? "rating"
+                    file_out(' }')
+                end
+            else
+                next
+            end
+        end
+        
+    end
+    file_out(" }")
+end
+
+def get_each_team()
+
+    prem_table = '//*[@id="standings-16368-content"]'
+    row_count = $browser.element(:xpath => prem_table).wait_until.trs.count
+    teams_links = {}
+
+    for i in 1..row_count do
+        team_path = $browser.element(:xpath => prem_table + '/tr[' + i.to_s + ']/td[2]/a')
+        team_name = team_path.inner_html
+        team_link = team_path.parent.a.href
+        teams_links.store(team_name, team_link)
+        #puts teams_links
+    end
+    return teams_links
+end
+
+#get_each_team()
+
+def get_each_player()
+
+    player_table = '//*[@id="player-table-statistics-body"]'
+    row_count = $browser.element(:xpath => player_table).wait_until.trs.count
+    player_links = {}
+
+    for i in 1..row_count do
+        player_path = $browser.element(:xpath => player_table + '/tr[' + i.to_s + ']/td[3]/a')
+        player_name = player_path.inner_html
+        player_link = player_path.parent.a.href
+        player_links.store(player_name, player_link)
+    end
+    #puts player_links
+    return player_links
+end
+
+number_of_teams = get_each_team().length
+teams = get_each_team()
+
+for i in 0...number_of_teams do
+    #puts number_of_teams
+    team = teams.keys[i]
+    link = teams.values[i]
+    #puts team
+    #puts link
+    $browser.goto(link)
+
+    file_out('{ "' + team + '": ')
+
+    #number_of_players = get_each_player().length
+    players = get_each_player()
+
+    for i in 0..1 do
+    #for i in 0...number_of_players do
+        player = players.keys[i]
+        player_link = players.values[i]
+        #puts player
+        #puts link
+        $browser.goto(player_link)
+        file_out('{ "' + player + '": ')
+        wait_click_e('//*[@id="player-tournament-stats-options"]/li[5]/a')
+        
+        select_stats_list("Accumulation", "Per 90 mins")
+        
+        select_stats_list("category","Tackles")
+        #begin
+        get_full_table()
+        #rescue
+            #sleep(1)
+            #retry
+        #end
+
+        
+        # select_stats_list("category","Interception")
+        # get_full_table()
+        
+        # select_stats_list("category","Cards")
+        # get_full_table()
+        
+        # select_stats_list("category","Offsides")
+        # get_full_table()       
+        
+        # select_stats_list("category","Clearances")
+        # get_full_table()
+        
+        # select_stats_list("category","Blocks")
+        # get_full_table()
+
+        # select_stats_list("category","Saves")
+        # get_full_table()
+
+        # select_stats_list("category","Shots")
+        # get_full_table()
+
+        # select_stats_list("category","Goals")
+        # get_full_table()
+
+        # select_stats_list("category","Dribbles")
+        # get_full_table()
+
+        # select_stats_list("category","Possession loss")
+        # get_full_table()
+
+        # select_stats_list("category","Aerial")
+        # get_full_table()
+
+        # select_stats_list("category","Passes")
+        # get_full_table()
+
+        # select_stats_list("category","Key passes")
+        # get_full_table()
+
+        select_stats_list("category","Assists")
+        get_full_table()
+        
+        file_out(" }")
+
+    end
+end
+$browser.close
+
+
+# =begin 
+# xpaths 
+# tackles dropdown    //*[@id="category"]
+# details tab         //*[@id="team-squad-stats-options"]/li[5]/a
+#                     <a href="#team-squad-stats-detailed">Detailed</a>
+#                     //*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]/tr[1]/td[3]/a
+#                     //*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]/tr[2]/td[3]/a
+# table               //*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]
+
+# //*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]/tr[2]
+# //*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]/tr[1]
+
+# def get_full_table()
+#     data_rows = $browser.tr(:xpath => '//*[@id="statistics-table-detailed"]//*[@id="player-table-statistics-body"]/tr')
+#     puts data_rows
+#     data = data_rows.map { |data_row| 
+#         puts data_row
+#         data_row.td.map(&:text)
+#     }
+#     puts data
+# end
+
+# =end
+
+
+# def get_player_link(xpath)
+#     row_count = $browser.element(:xpath => '//*[@id="player-table-statistics-body"]').trs.count
+#     puts row_count
+#     for i in 1..row_count do
+#         puts
+#         puts i
+#         each_player = '//*[@id="player-table-statistics-body"]/tr[' + i.to_s + ']/td[3]/a'
+#         #puts each_player
+#         player_name = $browser.element(:xpath => each_player)#.inner_html
+#         puts '{ Name: ' + player_name.inner_text + " }"
+        
+#         wait_click_e(each_player)
+        
+#         wait_click_e('//*[@id="player-tournament-stats-options"]/li[5]/a')
+        
+#         select_stats_list("Accumulation", "Per 90 mins")
+        
+#         select_stats_list("category","Tackles")
+#         get_full_table()
+        
+#         select_stats_list("category","Interception")
+#         get_full_table()
+        
+#         select_stats_list("category","Cards")
+#         get_full_table()
+        
+#         select_stats_list("category","Offsides")
+#         get_full_table()       
+        
+#         select_stats_list("category","Clearances")
+#         get_full_table()
+        
+#         select_stats_list("category","Blocks")
+#         get_full_table()
+
+#         select_stats_list("category","Saves")
+#         get_full_table()
+
+#         select_stats_list("category","Shots")
+#         get_full_table()
+
+#         select_stats_list("category","Goals")
+#         get_full_table()
+
+#         select_stats_list("category","Dribbles")
+#         get_full_table()
+
+#         select_stats_list("category","Possession loss")
+#         get_full_table()
+
+#         select_stats_list("category","Aerial")
+#         get_full_table()
+
+#         select_stats_list("category","Passes")
+#         get_full_table()
+
+#         select_stats_list("category","Key passes")
+#         get_full_table()
+
+#         select_stats_list("category","Assists")
+#         get_full_table()
+
+#         $browser.back
+        
+#     end
+# end
+
